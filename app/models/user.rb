7f42c898e63f1fa127fb4b5668cfb17a3b2baa20
dnnguyen35 <117180782+dnnguyen35@users.noreply.github.com>
2023-08-27 13:39:49 +0700

    Add sign in and sign up with devise (#9)
    * Add Signup and Signin feature (UI,function)
    
    * Refactor Code
    
    * Remove default values and wrong migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, on: :create
+  validates :password, confirmation: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+       
 end
