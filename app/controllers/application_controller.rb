7b32750d9ebf377c91e86d5c1bdffaa6391c7b4e
dnnguyen35 <117180782+dnnguyen35@users.noreply.github.com>
2023-10-22 22:14:00 +0700

    Add Edit, Update Personal Details feature (#14)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,5 +15,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) do |u|
       u.permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
+
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   root 'articles#index'
 
   resources :articles do
     resources :comments
   end
+
+  resources :users
 end
